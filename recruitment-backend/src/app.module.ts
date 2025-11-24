d604d955eee0822f86bcd7d6e9a379772c733f49
karmakandil <karmakandil2@gmail.com>
2025-11-24 03:20:40 +0200

    fixed errors in new recruitment folder

--- recruitment-backend/src/app.module.ts
+++ recruitment-backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+/*import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +7,32 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
+export class AppModule {}*/
+
+import { Module } from '@nestjs/common';
+import { RecruitmentModule } from './recruitment/recruitment.module';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+@Module({
+   imports: [
+    // Read .env variables globally (MONGODB_URI, PORT, etc.)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // Connect to MongoDB using env
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+  RecruitmentModule,
+   ],
+    controllers: [AppController],
+  providers: [AppService],
+
+})
 export class AppModule {}
+
