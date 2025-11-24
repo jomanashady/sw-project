2c9b4391d73a02eb2c176f751786c9606fbcd0d8
karmakandil <karmakandil2@gmail.com>
2025-11-24 15:55:36 +0200

    fix merge errors

--- recruitment-backend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
-  console.log(`Application is running on port ${process.env.PORT ?? 5000}`);
-}
-bootstrap();
