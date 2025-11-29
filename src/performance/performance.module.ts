de88b2df6e477ebabfcda80f4f00456d0f4bc851
Azzzamm <mohamed3azzam6@gmail.com>
2025-11-29 20:58:37 +0200

    backend

--- src/performance/performance.module.ts
+++ src/performance/performance.module.ts
@@ -1,3 +1,4 @@
+// src/performance/performance.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PerformanceController } from './performance.controller';
